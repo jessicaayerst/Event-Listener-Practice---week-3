a2d27d08cc32ba6b31edb9f7a5b42d6193bffeb1
newforce38 <jessicaayerst@gmail.com>
2019-08-19 13:18:00 -0400

    commit practice exercises completed

---
+++ scipts.js
@@ -0,0 +1,19 @@
+// const inputElement = document.querySelector("#message");
+// console.log(inputElement);
+
+
+
+
+// inputElement.addEventListener("keyup", function(){
+//    const inputElementValue = inputElement.value;
+
+//    const resultMessage = document.querySelector("#article-one")
+//    resultMessage.innerHTML = inputElementValue;
+
+//    const resultMessageTwo = document.querySelector("#article-two")
+//    resultMessageTwo.innerHTML = inputElementValue;
+
+
+// })
+
+
